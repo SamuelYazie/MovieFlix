24656491fcc225e41858ff02a1a9936fc47a900d
Samuel Yazie <samuel.yazie@abmtax.ca>
2026-05-05 16:43:44 -0500

    adding genres to the movies and the effect of taking you to the movie page on TMDB if you click the movie

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
   const [MovieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState();
+  const [genres, setGenres] = useState([]);
 
   useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
 
@@ -40,7 +41,6 @@ function App() {
         setErrorMessage(data.error || 'No movies found.');
       } else {
         setErrorMessage('');
-        console.log('Fetched movies:', data.results);
         setMovieList(data.results);
         return;
       }
@@ -52,9 +52,26 @@ function App() {
     }
   }
 
+  const fetchGenres = async () => {
+    try {
+      const response = await fetch(
+        `${API_BASE_URL}/genre/movie/list`,
+        API_OPTIONS
+      );
+
+      const data = await response.json();
+      setGenres(data.genres);
+    } catch (error) {
+      console.error("Failed to fetch genres", error);
+    }
+  };
+
   useEffect(() => {
     fetchMovies(debouncedSearchTerm);
   }, [debouncedSearchTerm]);
+  useEffect(() => {
+    fetchGenres();
+  }, []);
   return (
     <main>
       <div className='pattern' />
@@ -73,7 +90,7 @@ function App() {
           ) : (
             <ul>
               {MovieList.map((movie) => (
-                <MovieCard key={movie.id} movie={movie} />
+                <MovieCard key={movie.id} movie={movie} genres={genres}/>
               ))}
             </ul>
           )}
@@ -84,160 +101,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const Card = ({ title }) => {
-//   const [count, setCount] = useState(0);
-//   const [hasLiked, setHasLiked] = useState(false);
-
-//   useEffect(() => {
-//     console.log(`${title} has been ${hasLiked ? 'liked' : 'unliked'}`);
-//   }, [hasLiked]);
-
-//   return (
-//     <div className='card' onClick={() => setCount(count + 1)}>
-//       <h2>{title} <br/> {count}</h2>
-//       <button onClick={() => setHasLiked(!hasLiked)}>
-//         {hasLiked ? 'Liked' : 'Like'}
-//       </button>
-//     </div>
-//   )
-// }
-
-// const App = () => {
-//   return (
-//     <div className='card-container'>
-//       <Card title="The lion king" />
-//       <Card title="The dark knight" />
-//       <Card title="Gladiator" />
-//     </div>
-//   )
-// }
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <section id="center">
-//         <div className="hero">
-//           <img src={heroImg} className="base" width="170" height="179" alt="" />
-//           <img src={reactLogo} className="framework" alt="React logo" />
-//           <img src={viteLogo} className="vite" alt="Vite logo" />
-//         </div>
-//         <div>
-//           <h1>Get started</h1>
-//           <p>
-//             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-//           </p>
-//         </div>
-//         <button
-//           type="button"
-//           className="counter"
-//           onClick={() => setCount((count) => count + 1)}
-//         >
-//           Count is {count}
-//         </button>
-//       </section>
-
-//       <div className="ticks"></div>
-
-//       <section id="next-steps">
-//         <div id="docs">
-//           <svg className="icon" role="presentation" aria-hidden="true">
-//             <use href="/icons.svg#documentation-icon"></use>
-//           </svg>
-//           <h2>Documentation</h2>
-//           <p>Your questions, answered</p>
-//           <ul>
-//             <li>
-//               <a href="https://vite.dev/" target="_blank">
-//                 <img className="logo" src={viteLogo} alt="" />
-//                 Explore Vite
-//               </a>
-//             </li>
-//             <li>
-//               <a href="https://react.dev/" target="_blank">
-//                 <img className="button-icon" src={reactLogo} alt="" />
-//                 Learn more
-//               </a>
-//             </li>
-//           </ul>
-//         </div>
-//         <div id="social">
-//           <svg className="icon" role="presentation" aria-hidden="true">
-//             <use href="/icons.svg#social-icon"></use>
-//           </svg>
-//           <h2>Connect with us</h2>
-//           <p>Join the Vite community</p>
-//           <ul>
-//             <li>
-//               <a href="https://github.com/vitejs/vite" target="_blank">
-//                 <svg
-//                   className="button-icon"
-//                   role="presentation"
-//                   aria-hidden="true"
-//                 >
-//                   <use href="/icons.svg#github-icon"></use>
-//                 </svg>
-//                 GitHub
-//               </a>
-//             </li>
-//             <li>
-//               <a href="https://chat.vite.dev/" target="_blank">
-//                 <svg
-//                   className="button-icon"
-//                   role="presentation"
-//                   aria-hidden="true"
-//                 >
-//                   <use href="/icons.svg#discord-icon"></use>
-//                 </svg>
-//                 Discord
-//               </a>
-//             </li>
-//             <li>
-//               <a href="https://x.com/vite_js" target="_blank">
-//                 <svg
-//                   className="button-icon"
-//                   role="presentation"
-//                   aria-hidden="true"
-//                 >
-//                   <use href="/icons.svg#x-icon"></use>
-//                 </svg>
-//                 X.com
-//               </a>
-//             </li>
-//             <li>
-//               <a href="https://bsky.app/profile/vite.dev" target="_blank">
-//                 <svg
-//                   className="button-icon"
-//                   role="presentation"
-//                   aria-hidden="true"
-//                 >
-//                   <use href="/icons.svg#bluesky-icon"></use>
-//                 </svg>
-//                 Bluesky
-//               </a>
-//             </li>
-//           </ul>
-//         </div>
-//       </section>
-
-//       <div className="ticks"></div>
-//       <section id="spacer"></section>
-//     </>
-//   )
-// }
-
-// export default App
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
-function MovieCard({ movie: {original_title, vote_average, poster_path, release_date, original_language} }) {
+function MovieCard({ movie: {id, original_title, vote_average, poster_path, release_date, original_language, genre_ids}, genres }) {
+    const genreNames = genre_ids.map(id => {
+        const genre = genres.find(g => g.id === id);
+        return genre ? genre.name : 'Unknown';
+    }).join(', ');
+
     return (
-        <div className='movie-card'>
+        <div className='movie-card cursor-pointer' onClick={() => window.open(`https://www.themoviedb.org/movie/${id}`, '_blank')}>
             <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt={original_title} className='movie-poster' />
             <div>
-                <h3 className='mt-3'>{original_title}</h3>
-                <div className='content'>
+                <h3 className='mt-3 text-m'>{original_title}</h3>
+                <p className='text-white text-s mt-1.5'>{genreNames}</p>
+                <div className='content mt-0'>
                     <div className='rating'>
                         <img src='star.svg' alt='Star' />
                         <p className='text-white'>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
